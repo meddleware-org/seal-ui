d462ad120775b4d9cf624ccb7e209bd3e1ab595f
MeddleWare-Org <meddleware@proton.me>
2026-09-21 03:56:23 +0100

    Refine theme

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import '@meddleware/design-tokens/tokens.css'
+import '@meddleware/design-tokens/seasons.css'
 import '@meddleware/ui/base.css'
 import './styles.css'
+import { useSeason } from '@meddleware/ui'
 import App from './App.vue'
 
+useSeason()
 createApp(App).mount('#app')
